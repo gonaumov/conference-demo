07a64494f09d1f42afa4cd8f5faef9c1ab83bba2
George Naumov <gonaumov@gmail.com>
2022-03-05 23:02:48 +0200

    Fixing compatibility issue and implementing controllers.

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
@@ -2,11 +2,9 @@ package com.pluralsight.conferencedemo.controllers;
 
 import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,4 +24,27 @@ public class SessionController {
     public Session get(@PathVariable Long id) {
         return sessionRepository.getOne(id);
     }
+
+    @PostMapping
+    public Session create(@RequestBody final  Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        // Also need to check for children records before deleting
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        // because this is a PUT, we expect all attributes to be passed in.
+        // A PATH only need what.
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400
+        // bad payload
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
+
 }
--- src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
@@ -1,12 +1,11 @@
 package com.pluralsight.conferencedemo.controllers;
 
+import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.models.Speaker;
 import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,30 @@ public class SpeakerController {
     @RequestMapping("{id}")
     public Speaker get(@PathVariable Long id) {
         return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(
+       @RequestBody final Speaker speaker
+    ) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        // Also need to check for children records before deleting
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id,
+                          @RequestBody Speaker speaker) {
+        // because this is a PUT, we expect all attributes to be passed in.
+        // A PATH only need what.
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400
+        // bad payload
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
     }
 }
